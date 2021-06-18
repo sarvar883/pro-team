1d58b05bc90cbfa4009ef9fa1c01a9e0c49ccf3a
Sarvar <sarvarik883@gmail.com>
2021-06-18 15:18:24 +0500

    added build folder

---
+++ client/build/precache-manifest.1333b38db9e0ae6cd65ef8b1a7705b83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876c8e214ba8cd9a731a4c4093fc2c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89afaf5d857512d39d34",
+    "url": "/static/css/2.0106ae59.chunk.css"
+  },
+  {
+    "revision": "714fb4a80359db06b38d",
+    "url": "/static/css/main.dffa15af.chunk.css"
+  },
+  {
+    "revision": "89afaf5d857512d39d34",
+    "url": "/static/js/2.4e56301a.chunk.js"
+  },
+  {
+    "revision": "714fb4a80359db06b38d",
+    "url": "/static/js/main.3dfafc92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8d51a24e14d41b8a6f68448f635c544",
+    "url": "/static/media/white-spinner.f8d51a24.gif"
+  }
+]);
